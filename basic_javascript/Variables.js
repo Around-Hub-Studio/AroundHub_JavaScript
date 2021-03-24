61df3313b266f03ff74b63cc5e24aed0ae121e11
flature <jungwoo5394@gmail.com>
2021-03-24 20:40:37 +0900

    자바스크립트 강의 코드

---
+++ basic_javascript/Variables.js
@@ -0,0 +1,17 @@
+// 변수 선언 : 'let' 키워드
+let integerValue = 123;
+let stringValue = "문자열 변수";
+let booleanValue = true;
+
+console.log(integerValue);
+console.log(stringValue);
+console.log(booleanValue);
+
+// 상수 선언 : 'const' 키워드
+const integerConstant = 123;
+const stringConstant = "문자열 상수";
+const booleanConstant = true;
+
+console.log(integerConstant);
+console.log(stringConstant);
+console.log(booleanConstant);
